2ea35514a376e7e0c81e5f5f231af513d1ae9c33
Cayden Bernard <caydenbernard@live.com>
2025-01-03 17:58:28 -0500

    scripts to set up dataset

---
+++ src/EditDataset.py
@@ -0,0 +1,65 @@
+import ndjson
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageDraw, ImageEnhance
+import random
+
+def append_batch(file_path, batch_size):
+    with open(file_path) as f:
+        data = ndjson.load(f)
+
+    train_plus_test_batch_size = batch_size + (batch_size // 4)
+    length = len(data)
+    random_indices = random.sample(range(0, length), train_plus_test_batch_size)
+
+    for i in range(len(random_indices)):
+        drawing = data[random_indices[i]]['drawing']
+        label = data[random_indices[i]]['word']
+    
+        image = Image.new('L', (256, 256), 0)
+        draw = ImageDraw.Draw(image)
+        for stroke in drawing:
+            for j in range(len(stroke[0]) - 1):
+                x0, y0 = int(stroke[0][j] * 256 / 255), int(stroke[1][j] * 256 / 255)
+                x1, y1 = int(stroke[0][j + 1] * 256 / 255), int(stroke[1][j + 1] * 256 / 255)
+                draw.line([x0, y0, x1, y1], fill=255, width=2)
+    
+        resized_image = image.resize((32, 32), Image.BICUBIC)
+        enhancer = ImageEnhance.Contrast(resized_image)
+        enhanced_image = enhancer.enhance(5.0)
+        image_array = np.array(enhanced_image)
+
+        if i < batch_size:
+            train_dataset.append({"label": label, "image": image_array})
+        else:
+            test_dataset.append({"label": label, "image": image_array})
+        print(f"progress: {len(train_dataset)+len(test_dataset)} / {train_plus_test_batch_size * len(datasets)}", end="\r")
+
+#def display_image(image):
+    # Resize the image to 28x28 using bicubic interpolation
+  #  resized_image = image.resize((32, 32), Image.BICUBIC)
+    
+    # Enhance the contrast of the image
+ #   enhancer = ImageEnhance.Contrast(resized_image)
+   # enhanced_image = enhancer.enhance(5.0)  # Increase the contrast
+
+  #  image_array = np.array(enhanced_image)
+   # print(image_array)
+    
+    # Display the image using matplotlib
+  #  plt.imshow(enhanced_image, cmap='gray')
+  #  plt.axis('off')
+  #  plt.show()
+
+if __name__ == "__main__":
+    datasets = ["full_simplified_angel", "full_simplified_basketball", "full_simplified_car", "full_simplified_cat",
+                 "full_simplified_crab" , "full_simplified_dolphin", "full_simplified_helicopter", "full_simplified_mushroom",
+                 "full_simplified_octopus", "full_simplified_skull"]
+    train_dataset = []
+    test_dataset = []
+    for dataset in datasets:
+        file_path = f'DoodleData/{dataset}.ndjson'
+        append_batch(file_path, 10000)
+    np.save("Doodle_Data_Train.npy", train_dataset)
+    np.save("Doodle_Data_Test.npy", test_dataset)
+    
---
+++ src/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+loaded_data = np.load("Doodle_Data_Train.npy", allow_pickle=True).tolist()
+random_index = random.randint(0, len(loaded_data)-1)
+image_label = loaded_data[random_index]["label"]
+image_array = loaded_data[random_index]["image"]
+
+print(image_array)
+
+plt.imshow(image_array, cmap='gray', vmin=0, vmax=255)
+plt.title(image_label, fontsize=20)
+plt.axis('off')
+plt.show()
